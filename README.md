b3c7b8ca6bbd90133cfde091241ab843402c97a5
Mack Ward <mackorone@gmail.com>
2021-03-20 15:12:05 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ubmail-keep-alive
-Don't let my @buffalo.edu email address get garbage collected
+
+> Don't let my `buffalo.edu` email address get garbage collected
+
+## Introduction
+
+Every two months, I receive the following email:
+
+> Dear Mackenzie Ward,
+>
+> Our records indicate you have not logged in to your UBmail Powered by Google email in the last 60 days. You must log in using the link below at least once every 90 days to keep your UBmail service active.
+>
+> Please log in here: https://ubmail.buffalo.edu
+>
+> If you do not log into your UBmail for 90 days, you will lose access to your UBmail service. Any existing email messages you may have in your UBmail will eventually be lost.
+
+I'm tired of manually logging into my old email account just to save it from garbage collection.
+
+Thankfully, this chore can be automated.
---
+++ script.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import argparse
+import contextlib
+import logging
+import os
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
+logger = logging.getLogger(__name__)
+
+
+@contextlib.contextmanager
+def get_driver(executable_path):
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox(
+        options=options,
+        executable_path=executable_path,
+    )
+    try:
+        yield driver
+    finally:
+        driver.quit()
+
+
+def main():
+    parser = argparse.ArgumentParser(description="UBMail login script")
+    parser.add_argument("--webdriver-executable-path", required=True)
+    args = parser.parse_args()
+
+    logger.info("Reading credentials")
+    username = os.getenv("UBIT_USERNAME")
+    password = os.getenv("UBIT_PASSWORD")
+    assert username
+    assert password
+
+    logger.info("Starting WebDriver")
+    with get_driver(args.webdriver_executable_path) as driver:
+        logger.info("Going to UBMail page")
+        driver.get("https://ubmail.buffalo.edu/cgi-bin/login.pl")
+
+        logger.info("Waiting for redirect")
+        wait = WebDriverWait(driver, timeout=10)
+        wait.until(lambda x: x.find_element_by_id("login-button"))
+
+        logger.info("Submitting credentials")
+        driver.find_element_by_id("login").send_keys(username)
+        driver.find_element_by_id("password").send_keys(password)
+        driver.find_element_by_id("login-button").click()
+
+        logger.info("Waiting for authentication")
+        wait.until(EC.title_contains(f"{username}@buffalo.edu"))
+        logger.info("Success")
+
+
+if __name__ == "__main__":
+    main()
